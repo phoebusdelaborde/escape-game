596afe417481a6a355c6b234320afba9928084b5
phoebusdelaborde <128900017+phoebusdelaborde@users.noreply.github.com>
2024-05-27 20:11:59 +0200

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ game.addEventListener("click", function () {
   code = prompt("Code ?", ""); 
   if (code === "2121") {
     alert("Regardez la porte");
-  } else if(code != 2121{
+  } else if(code != 2121){
     alert("Gros mito");
   }
   else{

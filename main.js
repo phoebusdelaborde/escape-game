a076899617c0eac9ca1906e4468448360c7076e6
phoebusdelaborde <128900017+phoebusdelaborde@users.noreply.github.com>
2024-05-27 20:08:26 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 let game = document.getElementById("game");
 game.addEventListener("click", function () {
-  code = prompt("Code ?", " ");
+  code = prompt("Code ?", ""); 
   if (code === "2121") {
     alert("Regardez la porte");
-  } else {
+  } else if(code != 2121{
     alert("Gros mito");
+  }
+  else{
+    alert("Regardez la porte");
   }
 });
